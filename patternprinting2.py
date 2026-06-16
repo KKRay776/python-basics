165ab03791ef22e5ff47601f9d0f48fd504fd9c2
KKRay776 <thedeeeproot@gmail.com>
2026-06-16 20:21:58 +0530

    Add files via upload

---
+++ patternprinting2.py
@@ -0,0 +1,12 @@
+""" 1
+    1 2
+    1 2 3 2 1
+    1 2 3 4 3 2 1"""
+rows=int(input('enter the numbers of rows'))
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    for k in range(i-1,0,-1):
+        print(k,end='')
+
+    print( )
